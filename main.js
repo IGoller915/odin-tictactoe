fed20375033907a0335bf836102f83ff4b672dba
Ian Goller <ian.goller915@gmail.com>
2023-05-11 15:00:36 -0400

    add multiple round functionality

--- main.js
+++ main.js
@@ -1,26 +1,37 @@
 //factory for player creation
 const player = (token) => {
 
-    const getToken = () => token 
+    const getToken = token 
 
     return {getToken}
 }
 
 //module defining gameboard layout and behavior?
 const gameBoard = (() => {
-    const player1 = player('x')
-    const player2 = player('o')
+    const player1 = player('x');
+    const player2 = player('o');
+    let xScore = 0;
+    let oScore = 0;
+    let round = 1;
 
     //board tiles from top left to bottom right
-    let board = Array(9)
-
+    let board = Array(9);
+    
     let currentPlayer = player1
 
-    const changePlayer = () => {currentPlayer = currentPlayer === player1 ? player2 : player1}
+    document.querySelector('.current-player').textContent = currentPlayer.getToken.toUpperCase()
+    document.querySelector('.x-points').textContent = xScore
+    document.querySelector('.o-points').textContent = oScore
+
+
+    const changePlayer = () => {
+        gameBoard.currentPlayer = gameBoard.currentPlayer === player1 ? player2 : player1
+        document.querySelector('.current-player').textContent = gameBoard.currentPlayer.getToken.toUpperCase()
+    }
 
     const selectTile = (tile) => {
-        if (board[tile - 1] === undefined) {
-            board[tile - 1] = currentPlayer.getToken()
+        if (gameBoard.board[tile - 1] === undefined) {
+            gameBoard.board[tile - 1] = gameBoard.currentPlayer.getToken
             changePlayer()
         }
     }
@@ -38,35 +49,55 @@ const gameBoard = (() => {
     const winCheck = () => {
         for (let i = 0; i < winConditions.length; i++) {
             const [a, b, c] = winConditions[i]
-            if (board[a] && board[a] === board[b] && board[b] === board[c]) {
-                const winner = board[a]
+            if (gameBoard.board[a] && gameBoard.board[a] === gameBoard.board[b] && gameBoard.board[b] === gameBoard.board[c]) {
+                const winner = gameBoard.board[a]
                 endGame(winner)
+                winner == 'x' ? xScore++ : oScore++  
+                document.querySelector('.x-points').textContent = xScore
+                document.querySelector('.o-points').textContent = oScore
+                return
             }
         }
+
+        if (!gameBoard.board.includes(undefined)) {
+            const winner = 'tie'
+            endGame(winner)  
+            return
+        } 
     }
 
     const endGame = (winner) => {
         //display win message and new game button
         const winMessage = document.querySelector('.win-message')
-        winMessage.firstChild.data = `${winner.toUpperCase()} Wins!` //.firstChild is the text of the div, .textContent was overwriting the inner div
+
+        if (winner !== 'tie') {
+            winMessage.firstChild.data = `${winner.toUpperCase()} Wins!`  //.firstChild is the text of the div, .textContent was overwriting the inner div
+        } else {
+            winMessage.firstChild.data = 'Tie'
+        }
+
         winMessage.style.visibility = 'visible'
 
         //stop player inputs
-        displayController.stopTokenAdd(displayController.tiles)
+        displayController.stopTokenAdd()
 
     }
 
     const resetBoard = () => {
         gameBoard.board = Array(9)
-        const tiles = document.querySelectorAll('.board-tile')
-        tiles.forEach(tile => tile.textContent = '')
-        tiles.forEach(tile => tile.addEventListener('click', displayController.addToken))
-        document.querySelector('.win-message').style.visibility='hidden'
-        gameBoard.currentPlayer = player1
+        displayController.clearTiles()
+        displayController.enableTokenAdd()
+        
+        document.querySelector('.win-message').style.visibility = 'hidden'
+
+        round++
+        gameBoard.currentPlayer = round % 2 === 0 ? player2 : player1
+        document.querySelector('.current-player').textContent = gameBoard.currentPlayer.getToken.toUpperCase()
+        
     }
 
     const displayBoard = () => {
-        console.log(board)
+        console.log(gameBoard.board)
     }
     
 
@@ -78,31 +109,33 @@ const gameBoard = (() => {
 //module for display/gameplay handling
 const displayController = (() => {
     
-
-    
-    
-
     const addToken = (e) => {
         let tileObject = e.target
         let tileNumber = tileObject.getAttribute('data-index')
         gameBoard.selectTile(tileNumber)
-        tileObject.textContent = gameBoard.board[tileNumber - 1]
+        tileObject.textContent = gameBoard.board[tileNumber - 1].toUpperCase()
         gameBoard.winCheck()
     }
 
     let tiles = document.querySelectorAll(".board-tile")
     tiles.forEach(tile => tile.addEventListener('click', addToken))
 
-    const stopTokenAdd = (tiles) => {
+    const enableTokenAdd = () => {
+        tiles.forEach(tile => tile.addEventListener('click', addToken))
+    }
+
+    const stopTokenAdd = () => {
         tiles.forEach(tile => tile.removeEventListener('click', addToken))
     }
 
-    
+    const clearTiles = () => {
+        tiles.forEach(tile => tile.textContent='')
+    }
 
     const newGameButton = document.querySelector('.new-game-button')
     newGameButton.addEventListener('click', gameBoard.resetBoard)
 
-    return {tiles, addToken, stopTokenAdd}
+    return {tiles, addToken, enableTokenAdd, stopTokenAdd, clearTiles}
 })()
 
 
